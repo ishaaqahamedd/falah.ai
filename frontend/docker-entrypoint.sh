27f8942d8f68a2ab8153acb7e466b313b5d10589
ishaaqahamedd <ishaaq2905@gmail.com>
2026-03-16 00:06:03 +0530

    Fix:updated nignix proxy setup

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+export BACKEND_HOST=$(echo "$BACKEND_URL" | sed 's|https\?://||' | sed 's|/.*||')
+envsubst '${BACKEND_URL} ${BACKEND_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
